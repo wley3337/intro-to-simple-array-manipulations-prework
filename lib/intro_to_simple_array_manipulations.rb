172bbab64efa3cfecd03d63e16108fcb0a9fc5f4
wley3337@gmail.com <wley3337@gmail.com>
2018-05-02 23:16:37 +0000

    automatically backed up by learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,29 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+
+def using_pop(array)
+  array.pop()
+end
+
+def pop_with_args(array)
+  array.pop(2)
+end
+
+def using_shift(array)
+  array.shift() 
+end
+
+def shift_with_args(array)
+  array.shift(2)
+end
+
+def using_concat (array_one, array_two)
+  array_one.concat(array_two)
+end
+
